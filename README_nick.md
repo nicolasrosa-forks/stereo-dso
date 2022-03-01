7f58b310b512e79536d1f3696aa00fabed68bdda
nicolasrosa <nicolas.rosa@hotmail.com>
2022-03-01 16:34:43 -0300

    [Mod] Stereo DSO working on KITTI Odom Sequences

--- README_nick.md
+++ README_nick.md
@@ -1,2 +1,16 @@
 # Helpful links
 1. [Programmer Sought | Two steps to successfully run the kitti dataset on stereo-dso](https://icode.best/i/10556638978046)
+2. [DSO windowed optimization code(2)](https://www.bbsmax.com/A/obzbNka0zE/)
+
+3. https://www.cnblogs.com/JingeTU/p/8329780.html
+4. https://www.cnblogs.com/JingeTU/p/8395046.html
+5. https://www.cnblogs.com/JingeTU/p/8306727.html
+
+
+
+
+1. [DSO windowed optimization 公式](https://www-bbsmax-com.translate.goog/A/qVdeNYobzP/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=pt-BR&_x_tr_pto=wapp)
+2. [DSO windowed optimization code(1)](https://www-bbsmax-com.translate.goog/A/o75N2jNK5W/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=pt-BR&_x_tr_pto=wapp)
+3. [DSO windowed optimization code(2)](https://www-bbsmax-com.translate.goog/A/E35pno0Azv/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=pt-BR&_x_tr_pto=wapp)
+4. [DSO windowed optimization code(3)](https://www-bbsmax-com.translate.goog/A/obzbNka0zE/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=pt-BR&_x_tr_pto=wapp)
+5. [DSO windowed optimization code(4)](https://www-bbsmax-com.translate.goog/A/ZOJPERNo5v/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=pt-BR&_x_tr_pto=wapp)
--- run_sdso_kitti_odom_seqs.sh
+++ run_sdso_kitti_odom_seqs.sh
@@ -1,19 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 # Loop through KITTI Sequences 00-10
 max=5
 
-for i in 00 01 02 03 04 05 06 07 08 09 10;
+# kitti_seqs="00 01 02"
+# kitti_seqs="03"
+# kitti_seqs="04 05 06 07 08 09 10 11 12"
+kitti_seqs="00 01 02 03 04 05 06 07 08 09 10 11 12"
+
+for i in $kitti_seqs
 do
+    echo "[Running sequence $i...]"
     # Run each sequence 5 times
     for k in `seq 1 $max`
     do
-    echo "[$k/5] Running Stereo DSO on KITTI Odometry sequence $i..."
+        echo "[$k/5] Running Stereo DSO on KITTI Odometry sequence $i..."
 
-    # build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./kitti_camera.txt preset=0 mode=1
-    build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./kitti_camera.txt preset=0 mode=1 nogui=1 > /dev/null  # Silent
-
-    mv "results/result.txt" "results/result_sdso_kitti_seq${i}_${k}_raw.txt"
+        if [ "$i" = "00" ] || [ "$i" = "01" ] || [ "$i" = "02" ]
+        then
+            # build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./camera_kitti00-02.txt preset=0 mode=1
+            build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./camera_kitti00-02.txt preset=0 mode=1 nogui=1 > /dev/null  # Silent
+        elif [ "$i" = "03" ]
+        then
+            # build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./camera_kitti03.txt preset=0 mode=1
+            build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./camera_kitti03.txt preset=0 mode=1 nogui=1 > /dev/null  # Silent
+        elif [ "$i" = "04" ] || [ "$i" = "05" ] || [ "$i" - "06" ] || [ "$i" = "07" ] || [ "$i" = "08" ] || [ "$i" = "09" ] || [ "$i" = "10" ] || [ "$i" = "11" ] || [ "$i" = "12" ]
+        then
+            # build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./camera_kitti04-12.txt preset=0 mode=1
+            build/bin/dso_dataset files=/media/nicolas/nicolas_seagate/datasets/kitti/odometry/dataset/sequences/$i calib=./camera_kitti04-12.txt preset=0 mode=1 nogui=1 > /dev/null  # Silent
+        fi
+        mv "results/result.txt" "results/result_sdso_kitti_seq${i}_${k}_raw.txt"
     done
     echo
 done
